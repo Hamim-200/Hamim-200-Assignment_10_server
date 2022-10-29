ad2fd6401f46455f717e82520042568f9340cf39
Hamim Ahmed <106913071+Hamim-200@users.noreply.github.com>
2022-10-29 22:14:58 +0600

    Add files via upload
    server update

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-
 const categories = require('./data/categories.json');
 const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
-    res.send('News Api Running')
+    res.send('Api Running');
 });
 
 app.get('/news-categories', (req, res) => {
@@ -19,26 +18,16 @@ app.get('/news-categories', (req, res) => {
 
 app.get('/category/:id', (req, res) => {
     const id = req.params.id;
-    if (id === '08') {
-        res.send(news);
-    }
-    else {
-        const category_news = news.filter(n => n.category_id === id);
-        res.send(category_news);
-    }
-})
-
-app.get('/news', (req, res) => {
-    res.send(news);
+    const category_news = news.filter(n => n.category_id === id);
+    res.send(category_news);
 })
 
 app.get('/news/:id', (req, res) => {
     const id = req.params.id;
-    const selectNews = news.find(n => n._id === id);
-    res.send(selectNews);
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
 })
 
 app.listen(port, () => {
-    console.log('Assignemnet Running on port', port);
-
+    console.log('Web hero server running on port ', port)
 });
